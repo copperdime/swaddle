cfd24d1d51331dffad41e1f0c59777aafc54ecdc
Adrianne Kelly <mrs.kelly@gmail.com>
2017-10-07 06:55:44 -0700

    Update plaid.js

--- config/plaid.js
+++ config/plaid.js
@@ -1,8 +1,8 @@
 module.exports = {
 
-    PLAID_CLIENT_ID : "59cbf7a9bdc6a40ac87ed8bb",
-    PLAID_SECRET : "31d69576685933df6ef151e7806b56",
-    PLAID_PUBLIC_KEY : "be508262813de7f2a74af25ff88055",
+    PLAID_CLIENT_ID : "",
+    PLAID_SECRET : "",
+    PLAID_PUBLIC_KEY : "",
     PLAID_ENV : "development",
-    ACCESS_TOKEN : "access-development-9b494732-a83d-4312-acac-f8c732070421"
+    ACCESS_TOKEN : ""
 };
