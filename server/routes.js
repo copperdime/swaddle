a194ce6350d18e159b76e7f50c5780c1a70691f6
copperdime <mrs.kelly@gmail.com>
2017-10-07 01:13:00 -0700

    Reestablishing route declaration in server.js/routes.js

--- server/routes.js
+++ server/routes.js
@@ -5,7 +5,7 @@ module.exports = function(app, passport) {
 
     // show the home page (will also have our login links)
     app.get('/', function(req, res) {
-       // res.render('index.ejs');
+       res.render('index.ejs');
     });
 
 
